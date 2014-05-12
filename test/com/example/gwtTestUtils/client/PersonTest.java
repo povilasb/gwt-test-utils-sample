61fca4fbc797dc369826509ba4ac83085836f10d
Povilas Balciunas <balciunas90@gmail.com>
2014-05-12 11:22:07 +0300

    Updates Person test to include code neccassary for using custom test runner.

--- test/com/example/gwtTestUtils/client/PersonTest.java
+++ test/com/example/gwtTestUtils/client/PersonTest.java
@@ -2,12 +2,16 @@ package com.example.gwtTestUtils.client;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
 
 import com.googlecode.gwt.test.GwtTest;
 import com.googlecode.gwt.test.GwtModule;
 
+import com.example.test.MyGwtRunner;
+
 
 @GwtModule("com.example.gwtTestUtils.Main")
+//@RunWith(MyGwtRunner.class)
 public class PersonTest extends GwtTest {
 
 	@Test
