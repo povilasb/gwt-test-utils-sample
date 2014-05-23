eebf347d2512fe23d4fe22c89da9eefea3025f48
Povilas Balciunas <balciunas90@gmail.com>
2014-05-23 14:28:44 +0300

    Adds toJson() method to Person class.

--- src/com/example/gwtTestUtils/client/Person.java
+++ src/com/example/gwtTestUtils/client/Person.java
@@ -1,5 +1,9 @@
 package com.example.gwtTestUtils.client;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONNumber;
+
 
 public class Person {
 
@@ -8,6 +12,10 @@ public class Person {
 
 
 	public Person(String name, int age) {
+		if (name == null) {
+			throw new NullPointerException("Name cannot be null.");
+		}
+
 		this.name = name;
 		this.age = age;
 	}
@@ -20,4 +28,15 @@ public class Person {
 		return this.age;
 	}
 
+	public JSONObject toJson() {
+		JSONObject result = new JSONObject();
+
+		JSONString jsonName = new JSONString(this.name);
+		result.put("name", jsonName);
+
+		JSONNumber jsonAge = new JSONNumber(this.age);
+		result.put("age", jsonAge);
+
+		return result;
+	}
 }
