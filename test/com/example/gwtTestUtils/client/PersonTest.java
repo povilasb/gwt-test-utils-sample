385f15c61dae5dae708fb2f059c5aeff8695f177
Povilas Balciunas <balciunas90@gmail.com>
2014-05-23 14:28:58 +0300

    Adds Person tests.

--- test/com/example/gwtTestUtils/client/PersonTest.java
+++ test/com/example/gwtTestUtils/client/PersonTest.java
@@ -1,24 +1,44 @@
 package com.example.gwtTestUtils.client;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 
 import com.googlecode.gwt.test.GwtTest;
 import com.googlecode.gwt.test.GwtModule;
 
-import com.example.test.MyGwtRunner;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONValue;
 
 
 @GwtModule("com.example.gwtTestUtils.Main")
-//@RunWith(MyGwtRunner.class)
 public class PersonTest extends GwtTest {
 
 	@Test
-	public void simpleTest() {
-		Person p = new Person("Povilas", 21);
-		assertEquals("Povilas", p.getName());
-		assertEquals(21, p.getAge());
+	public void testCreate() {
+		Person person = new Person("Povilas", 23);
+		assertTrue(person.getName() == "Povilas");
+		assertTrue(person.getAge() == 23);
 	}
 
+	@Test
+	public void testToJson() {
+		Person person = new Person("Povilas", 23);
+		JSONObject jsonPerson = person.toJson();
+		assertTrue(jsonPerson != null);
+
+		JSONValue jsonValue = jsonPerson.get("name");
+		assertTrue(jsonValue != null);
+		JSONString jsonName = jsonValue.isString();
+		assertTrue(jsonName != null);
+		assertTrue(jsonName.stringValue() == "Povilas");
+
+		jsonValue = jsonPerson.get("age");
+		assertTrue(jsonValue != null);
+		JSONNumber jsonAge = jsonValue.isNumber();
+		assertTrue(jsonAge != null);
+		assertTrue(jsonAge.doubleValue() == 23);
+	}
 }
